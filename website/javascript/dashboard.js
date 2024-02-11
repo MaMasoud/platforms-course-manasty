f38a4033f276f83c76a5ba185d78384a382ede1b
MaMasoud <hpdhooda@gmail.com>
2024-02-11 16:27:06 +0200

    a

--- website/javascript/dashboard.js
+++ website/javascript/dashboard.js
@@ -98,3 +98,38 @@ listElements.forEach(listElement => {
 document.addEventListener('contextmenu', function (e) {
     e.preventDefault();
 });
+
+
+
+
+
+
+// Get the modal
+var modal = document.getElementById('modal');
+
+// Get the button that opens the modal
+var openModalBtn = document.getElementById("openModalBtn");
+
+// Get the <span> element that closes the modal
+var closeBtn = document.getElementsByClassName("close")[0];
+var closeBtn2 = document.getElementsByClassName("close")[1];
+
+// When the user clicks the button, open the modal 
+openModalBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+closeBtn2.onclick = function() {
+    modal.style.display = "none";
+    }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
